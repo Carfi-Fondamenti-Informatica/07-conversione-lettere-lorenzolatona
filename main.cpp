267fcb587f6b81bcdd03a394652badd0dbeec3fc
lorenzolatona <115403824+lorenzolatona@users.noreply.github.com>
2022-12-06 10:29:42 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "lib.h"
+
+using namespace std;
 
 int main(){
+  char x;
+  cin>>x;
+  if(convert(x)){
+    cout<<x<<endl;
+  }else{
+    cout<<"errore"<<endl;
+  }
   return 0;
 }
